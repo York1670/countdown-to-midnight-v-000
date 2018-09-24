65dad4226febeb428c15403ab43e695b05f17421
Bobbi York <york1670@gmail.com>
2018-09-24 23:06:22 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,15 @@ def countdown(number)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(countdown)
+	
+	sleep_number = 1
+
+	until sleep_number > 10
+		puts countdown
+		sleep_number += 1
+		sleep(1)
+	end
+
+end
